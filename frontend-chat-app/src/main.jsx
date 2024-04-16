8c3db88826becf162d8d96b69355b9d629fff0a8
Hitendra <hitendraverma001@gmail.com>
2024-04-16 16:16:09 +0530

    added view chat info and upload group profile features

--- frontend-chat-app/src/main.jsx
+++ frontend-chat-app/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Provider } from "react-redux";
 import store from "./store/index.js";
 import SocketProvider from "./contexts/SocketProvider.jsx";
@@ -26,7 +26,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <BrowserRouter>
           <QueryClientProvider client={queryClient}>
             <App />
-            <ReactQueryDevtools initialIsOpen={false} />
+            {/* <ReactQueryDevtools initialIsOpen={false} /> */}
           </QueryClientProvider>
         </BrowserRouter>
       </SocketProvider>
